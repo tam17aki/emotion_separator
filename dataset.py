b64e159d22ab3d184fec692a23a853bd0635234b
Akira Tamamori <tamamori5917@gmail.com>
2023-07-26 18:59:56 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -21,6 +21,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+import os
+
+import joblib
 import torch
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -60,33 +63,68 @@ def get_dataloader(cfg, feats, labels):
     """Get Dataloaders for training and test.
 
     Args:
-        inputs (Tensor) : xvector [batch_size, 512]
-        labels (Tensor): emotion label [batch_size]
+        inputs (numpy array) : x-vectors
+        labels (numpy array): emotion label
 
     Returns:
         train_dataloader : dataloader for training
         test_dataloader : dataloader for test
     """
-    x_train, x_test, y_train, y_test = train_test_split(
-        feats,
-        labels,
-        test_size=cfg.training.test_size,
-        random_state=cfg.training.seed,
-    )
-    scaler = StandardScaler()
-    x_train_std = scaler.fit_transform(x_train)
-    x_test_std = scaler.transform(x_test)
+    if cfg.training.test_size != 0.0:
+        x_train, x_test, y_train, y_test = train_test_split(
+            feats,
+            labels,
+            test_size=cfg.training.test_size,
+            random_state=cfg.training.seed,
+        )
+        scaler = StandardScaler()
+        x_train_std = scaler.fit_transform(x_train)
+        x_test_std = scaler.transform(x_test)
+        train_dataloader = torch.utils.data.DataLoader(
+            dataset=MyDataset(x_train_std, y_train),
+            batch_size=cfg.training.n_batch,
+            shuffle=True,
+            drop_last=True,
+        )
+        test_dataloader = torch.utils.data.DataLoader(
+            dataset=MyDataset(x_test_std, y_test),
+            batch_size=cfg.training.n_batch,
+            shuffle=False,
+            drop_last=False,
+        )
+    else:
+        scaler = StandardScaler()
+        x_train_std = scaler.fit_transform(feats)
+        train_dataloader = torch.utils.data.DataLoader(
+            dataset=MyDataset(x_train_std, labels),
+            batch_size=cfg.training.n_batch,
+            shuffle=True,
+            drop_last=True,
+        )
+        test_dataloader = None
+    stats_dir = os.path.join(cfg.xvector.root_dir, cfg.xvector.stats_dir)
+    os.makedirs(stats_dir, exist_ok=True)
+    joblib.dump(scaler, os.path.join(stats_dir, cfg.training.scaler_file))
+    return {"train": train_dataloader, "test": test_dataloader}
 
-    train_dataloader = torch.utils.data.DataLoader(
-        dataset=MyDataset(x_train_std, y_train),
-        batch_size=cfg.training.n_batch,
-        shuffle=True,
-        drop_last=True,
-    )
-    test_dataloader = torch.utils.data.DataLoader(
-        dataset=MyDataset(x_test_std, y_test),
-        batch_size=cfg.training.n_batch,
+
+def get_dataloader_inference(cfg, feats, labels):
+    """Get Dataloaders for inference.
+
+    Args:
+        inputs (numpy array) : x-vectors
+        labels (numpy array): emotion label
+
+    Returns:
+        dataloader : dataloader for inferencef
+    """
+    stats_dir = os.path.join(cfg.xvector.root_dir, cfg.xvector.stats_dir)
+    scaler = joblib.load(os.path.join(stats_dir, cfg.training.scaler_file))
+    feats_std = scaler.transform(feats)
+    dataloader = torch.utils.data.DataLoader(
+        dataset=MyDataset(feats_std, labels),
+        batch_size=cfg.inference.n_batch,
         shuffle=False,
         drop_last=False,
     )
-    return {"train": train_dataloader, "test": test_dataloader}
+    return dataloader
